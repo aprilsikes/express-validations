ea20d8bea8cbb4a7bac08904d3f2a2efe89710ba
april sikes <aprilsikes@gmail.com>
2016-01-22 13:42:34 -0700

    name cannot be blank stretch suite

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var validate = require('../lib/validations');
 
-router.get('/', function(req, res, next) {  
+router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.post('/', function (req, res, next) {
+  var errors = [];
+  errors.push(validate.nameIsNotBlank(req.body.name));
+  errors.push(validate.emailIsValid(req.body.email));
+  errors.push(validate.phoneIsValid(req.body.phone));
+  console.log(errors);
+  errors = errors.filter(function (error) {
+    if (errors.length) {
+      return errors.length;
+      res.render('index');
+    }
+    else {
+      res.send('You are so valid');
+    }
+  })
+})
 
 module.exports = router;
